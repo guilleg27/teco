f1763a2e82ffe90734850ee9e7806be8bb3120d2
GuillermoOjeda <guillermo.ojeda@kickads.mobi>
2019-11-05 17:22:36 -0300

    tag analytics

--- views/formulario/promo20gb.php
+++ views/formulario/promo20gb.php
@@ -24,6 +24,15 @@ use yii\helpers\Url;
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-132110118-2"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-132110118-2');
+    </script>
     <!-- <script src="https://www.google.com/recaptcha/api.js?hl=es"></script> -->
     <script type="text/javascript">
       var verifyCallback4 = function(response) {

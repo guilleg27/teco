26b8e88cddc08a2a7810ff6347ff86ba7bb9d26f
GuillermoOjeda <guillermo.ojeda@kickads.mobi>
2020-06-29 15:40:34 -0300

    gracias test

--- views/formulario/gracias.php
+++ views/formulario/gracias.php
@@ -99,30 +99,6 @@ use yii\widgets\ActiveForm;
 
 <div class="pixel">
 
-    <?php $form = ActiveForm::begin(); ?>
-
-        <?= $form->field($model, 'nombre_completo') ?>
-        <?= $form->field($model, 'email') ?>
-        <?= $form->field($model, 'celular') ?>
-        <?= $form->field($model, 'date') ?>
-        <?= $form->field($model, 'ktoken') ?>
-        <?= $form->field($model, 'plan') ?>
-        <?= $form->field($model, 'pais') ?>
-        <?= $form->field($model, 'ciudad') ?>
-        <?= $form->field($model, 'carrier') ?>
-        <?= $form->field($model, 'pubId') ?>
-        <?= $form->field($model, 'providerId') ?>
-    
-        <div class="form-group">
-            <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
-        </div>
-    <?php ActiveForm::end(); ?>
-
-</div><!-- pixel -->
-
-
-<div class="pixel">
-
 Gracias.
 
 </div><!-- pixel -->

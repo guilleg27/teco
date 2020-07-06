cbecebbd57ab4cab44a003915ca8721748bb9c71
GuillermoOjeda <guillermo.ojeda@kickads.mobi>
2020-07-06 14:53:56 -0300

    prueba

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -32,8 +32,8 @@ AppAsset::register($this);
 </head>
 <body>
 <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NJS6H5K"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NJS6H5K"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript> -->
 <!-- End Google Tag Manager (noscript) -->
 <?php $this->beginBody() ?>
 
